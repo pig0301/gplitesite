0c2fa22ffd407217375bf63cc6e11898f75c6b72
GPLite <GPLite@KIMMY-X250>
2018-10-11 15:19:25 +0800

    dbconfig

--- blogsite/apps/home/views.py
+++ blogsite/apps/home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from dj_database_url import config
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
+    return HttpResponse(config())
+
 
