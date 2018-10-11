dc679de7a268ad354e2c51f501ba47fe509dde42
GPLite <GPLite@KIMMY-X250>
2018-10-11 16:36:24 +0800

    路径test

--- blogsite/apps/home/views.py
+++ blogsite/apps/home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+import sys
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return HttpResponse('\n'.join(sys.path))
--- gplitesite/urls.py
+++ gplitesite/urls.py
@@ -6,14 +6,14 @@ admin.autodiscover()
 
 import blogsite.views
 
-import blogsite.apps.home.views as home
+from home import views
 
 # Examples:
 # url(r'^$', 'gplitesite.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', home.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^db', blogsite.views.db, name='db'),
     path('admin/', admin.site.urls),
 ]
