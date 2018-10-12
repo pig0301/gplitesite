878e2bb1fd06650f4278fded3f691a82aa9842d3
GPLite <GPLite@KIMMY-X250>
2018-10-12 09:30:55 +0800

    调整目录

--- gplitesite/settings.py
+++ gplitesite/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogsite'
+    'htmlfiles'
 ]
 
 MIDDLEWARE = [
@@ -121,9 +121,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
--- gplitesite/urls.py
+++ gplitesite/urls.py
@@ -6,9 +6,6 @@ admin.autodiscover()
 
 import home.views as home
 
-# Examples:
-# url(r'^$', 'gplitesite.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
     url(r'^$', home.index, name='index'),
