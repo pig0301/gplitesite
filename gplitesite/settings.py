90601a787d14d0ca750c0d46b86ba28453741bb7
GPLite <GPLite@KIMMY-X250>
2018-10-12 09:36:56 +0800

    试试

--- gplitesite/settings.py
+++ gplitesite/settings.py
@@ -29,7 +29,7 @@ sys.path.append(os.path.join(BASE_DIR, 'blogsite'))
 SECRET_KEY = 'CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead).'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
