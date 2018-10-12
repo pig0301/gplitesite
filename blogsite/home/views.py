b9b8060a2713dcfdf703402a1be07c94de9c8d0b
GPLite <GPLite@KIMMY-X250>
2018-10-12 12:14:39 +0800

    icons

--- blogsite/home/views.py
+++ blogsite/home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+import settings
 
 
 def index(request):
-    return render(request, 'index.html')
+    data = {'IS_DEBUG': settings.DEBUG}
+    
+    return render(request, 'index.html', data)
--- gplitesite/urls.py
+++ gplitesite/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import url
 from django.urls import path
 
+from django.views.static import serve
 from django.contrib import admin
 admin.autodiscover()
 
 import home.views as home
+import settings
 
 
 urlpatterns = [
     url(r'^$', home.index, name='index'),
+    
     path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve, {"document_root": settings.MEDIA_ROOT}),
 ]
