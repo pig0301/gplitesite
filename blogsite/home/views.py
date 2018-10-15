6014a2a1d45563b9db4170419e008bc69c1f5174
GPLite <GPLite@KIMMY-X250>
2018-10-15 14:08:06 +0800

    登录搞定

--- blogsite/home/views.py
+++ blogsite/home/views.py
@@ -1,5 +1,33 @@
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.contrib import messages
+
 from libs.utils import render_template
 
 
 def index(request):
     return render_template('index.html', {}, request)
+
+def login(request):
+    username = request.POST['user'];
+    password = request.POST['password']
+    redirect_url = request.GET['r']
+
+    user = auth.authenticate(username=username, password=password)
+
+    if username == "" or password == "":
+        messages.warning(request, "Login failed. User name and password can not be blank.")
+    elif user is not None:
+        auth.login(request, user)
+    else:
+        messages.error(request, "Login failed. User name or password is wrong!", extra_tags="danger")
+
+    return HttpResponseRedirect(redirect_url)
+
+def logout(request):
+    redirect_url = request.GET['r']
+
+    auth.logout(request)
+    messages.info(request, "Logout successfully!")
+
+    return HttpResponseRedirect(redirect_url)
--- gplitesite/urls.py
+++ gplitesite/urls.py
@@ -11,7 +11,10 @@ import settings
 
 urlpatterns = [
     url(r'^$', home.index, name='index'),
-    
+
+    url(r'^login/$', home.login),
+    url(r'^logout/$', home.logout),
+
     path('admin/', admin.site.urls),
     url(r'^media/(?P<path>.*)$', serve, {"document_root": settings.MEDIA_ROOT}),
 ]
