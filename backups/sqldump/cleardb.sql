781d055bf3f7a5a2697692b4b1abd3a910f06667
GPLite <GPLite@KIMMY-X250>
2018-10-16 13:59:54 +0800

    sqldump

---
+++ backups/sqldump/cleardb.sql
@@ -0,0 +1,19 @@
+drop table public.auth_user_groups;
+drop table public.auth_user_user_permissions;
+drop table public.auth_group_permissions;
+drop table public.auth_permission;
+drop table public.auth_group;
+
+drop table public.django_admin_log;
+drop table public.django_content_type;
+drop table public.django_migrations;
+drop table public.django_session;
+
+drop table public.auth_user;
+
+drop table public.snooker_campaign;
+drop table public.snooker_cue;
+drop table public.snooker_frame;
+drop table public.snooker_group;
+drop table public.snooker_gym;
+drop table public.snooker_player;
