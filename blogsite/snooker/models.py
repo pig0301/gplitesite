8a7ae4a3148e810aaa9af7460dc1ff248fee04da
GPLite <GPLite@KIMMY-X250>
2018-10-16 10:01:17 +0800

    models

---
+++ blogsite/snooker/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+
+from django.db.models.aggregates import Avg, Sum, Max, Count
+from django.db.models import F
+
+
+class group(models.Model):
+    name = models.CharField(max_length=45)
+    is_show = models.CharField(max_length=1)
+    
+    def __unicode__(self):
+        return self.name
+    
+class player(models.Model):
+    name = models.CharField(max_length=45)
+    group = models.ForeignKey(group, on_delete=models.DO_NOTHING, related_name='group_player')
+
+    def __unicode__(self):
+        return self.name
+
+class gym(models.Model):
+    name = models.CharField(max_length=45)
+    road = models.CharField(max_length=45)
+
+    def __unicode__(self):
+        return self.name + '(' + self.road + ')'
+
+class cue(models.Model):
+    name = models.CharField(max_length=45)
+    material = models.CharField(max_length=45)
+    owner = models.OneToOneField(player, on_delete=models.DO_NOTHING)
+
+    def __unicode__(self):
+        return self.name + '(' + self.owner.name + ')'
+
+class campaign(models.Model):
+    event_dt = models.DateField()
+    gym = models.OneToOneField(gym, on_delete=models.DO_NOTHING)
+    cue = models.OneToOneField(cue, on_delete=models.DO_NOTHING)
+    opponent = models.OneToOneField(player, on_delete=models.DO_NOTHING)
+    let_points = models.IntegerField()
+    is_finished = models.CharField(max_length=1)
+
+class frame(models.Model):
+    campaign = models.ForeignKey(campaign, on_delete=models.DO_NOTHING, related_name='campaign_frame')
+    cue = models.OneToOneField(cue, on_delete=models.DO_NOTHING)
+    let_points = models.IntegerField()
+    get_points = models.IntegerField()
+    oppo_points = models.IntegerField()
+    is_win = models.CharField(max_length=1)
+    max_break = models.IntegerField()
+    memo = models.CharField(max_length=200)
+
+
+DEFAULT_CAMPAIGN_ANNOTATE = {
+    'win_frames': Sum('campaign_frame__is_win'),
+    'lose_frames': Sum(1 - F('campaign_frame__is_win')),
+    'avg_get_points': Avg('campaign_frame__get_points'),
+    'avg_oppo_points': Avg('campaign_frame__oppo_points'),
+    'campaign_break': Max('campaign_frame__max_break')
+}
+
+
+DEFAULT_FRAME_ANNOTATE = {
+    'win_frames': Sum('is_win'),
+    'lose_frames': Sum(1 - F('is_win')),
+    'avg_get_points': Avg('get_points'),
+    'avg_oppo_points': Avg('oppo_points'),
+    'frame_count': Count('id'),
+    'campaign_count': Count('campaign', distinct=True),
+    'maximum_break': Max('max_break')
+}
