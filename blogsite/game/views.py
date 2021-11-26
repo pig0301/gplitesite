9db07bc360853773e926967f39a79a48dbcbcd23
GPLite <GPLite@KIMMY-DESKTOP>
2021-11-27 00:21:14 +0800

    减少重复

--- blogsite/game/views.py
+++ blogsite/game/views.py
@@ -11,12 +11,22 @@ def index(request):
 def produce_start(request):
     warehouse = request.GET['w']
     main_chef = request.GET['c']
- 
-    produce = models.cg_mp700_produce(warehouse=warehouse, main_chef=main_chef)
-    produce.save()
-   
+    round1 = request.GET['r']
+
+    produce = None
+    detail = None
+    details = models.cg_mp700_detail.objects.prefetch_related('produce').order_by("-id")
+
+    if details.count() > 0:
+        detail = details.first()
+        produce = detail.produce
+    
+    if produce is None or detail.end_dttm is not None or produce.warehouse != warehouse or produce.main_chef != main_chef or int(round1) < detail.round or int(round1) - detail.round > 1:
+        produce = models.cg_mp700_produce(warehouse=warehouse, main_chef=main_chef)
+        produce.save()
+    
     models.cg_mp700_detail.objects.filter(end_dttm=None).update(end_dttm=timezone.now())
-   
+
     return HttpResponse(produce.id)
 
 def produce_finish(request, produce_id):
@@ -39,7 +49,7 @@ def finish_last_step(produce_id):
     produce = models.cg_mp700_produce.objects.prefetch_related('produce_detail').get(id=produce_id)
     lastStep = produce.produce_detail.last()
    
-    if lastStep is not None:
+    if lastStep is not None and lastStep.end_dttm is None:
         lastStep.end_dttm = timezone.now()
         lastStep.save()
    
