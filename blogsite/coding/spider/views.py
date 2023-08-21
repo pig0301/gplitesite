b1419a1da82ee5acb598a3186abea50f63b96c53
GPLite <GPLite@KIMMY-X1>
2023-08-21 15:02:40 +0800

    还是得用selenium查库存

--- blogsite/coding/spider/views.py
+++ blogsite/coding/spider/views.py
@@ -1,7 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
 from django.http import HttpResponse
 from bs4 import BeautifulSoup
 
-import requests, re, json, datetime
+import re, json, datetime
 
 from libs.functions import render_template, check_login, get_client_ip
 from libs import wechat, dingding
@@ -9,43 +12,82 @@ from libs import wechat, dingding
 
 def query_storage(request):
     if check_login(request):
-        link_id = "9003867817"
-        spec_map = {
-            '050100001': 5, '050100002': 10, '050100003': 20,
-            '000010050': 50, '000010100': 100, '000010200': 200
-        }
+        options = webdriver.FirefoxOptions()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
         
-        url = 'https://mall.icbc.com.cn/products/pd_' + link_id + '.jhtml'
-        response = requests.get(url)
-        html = response.content
+        browser = webdriver.Firefox(executable_path="/data/firefox/geckodriver", options=options)
         
-        soup = BeautifulSoup(html, 'html.parser')
-        pattern = re.compile(r'var prodSkuJson = (.*?);')
+        prod_links = ['9003867817', '9002749021']
         
-        script = soup.find(id='prodSkuDiv').find('script')
-        products = json.loads(pattern.search(script.prettify()).group(1))
-        products = sorted(products, key=lambda x: spec_map[x['merchantProdId'].rjust(9, '0')])
-        
+        storage_re = re.compile(r'\s(\d+)\s')
         storage_warn = ""
         
-        for product in products:
-            product['merchantProdId'] = product['merchantProdId'].rjust(9, '0')
-            product['spec'] = spec_map[product['merchantProdId']]
+        for link_id in prod_links:
+            url = 'https://mall.icbc.com.cn/products/pd_' + link_id + '.jhtml'
+            browser.get(url)
             
-            product['name'] = "“如意金条”" + str(product['spec']) + "g"
-            product['prodUrl'] = url
-            storage = product['totalStorage']
+            WebDriverWait(browser, 5).until(lambda x: x.find_element_by_id('skuSelected').is_displayed())
+            soup = BeautifulSoup(browser.page_source, 'html.parser')
             
-            if int(storage) <= 20:
-                storage_warn += "\n" + product['name'] + "仅剩" + storage + "件。"
-                product['is_warning'] = 1
-        
+            re_results = [None, None, None, None]
+            re_exps = [
+                re.compile(r'var proName = (.*?);'),
+                re.compile(r'var skuToPropJson = (.*?);'),
+                re.compile(r'var prodSkuJson = (.*?);'),
+                re.compile(r'var prodEnumMap = (.*?);')
+            ]
+                
+            for script in soup.find_all('script'):
+                for i in range(len(re_results)):
+                    match_exp = re_exps[i].search(script.prettify())
+                    if match_exp:
+                        re_results[i] = json.loads(match_exp.group(1))
+             
+            (prod_name, prods_sku, prods_info, prods_spec) = re_results
+             
+            prods_info = sorted(prods_info, key=lambda x: x['prodSkuId'])
+            
+            for product in prods_info:
+                product['merchantProdId'] = product['merchantProdId'].rjust(9, '0')
+                product['sku_info'] = prods_sku[product['prodSkuId']][0]
+                product['name'] = prod_name + prods_spec[product['sku_info']['prodEnumId']]
+                product['prodUrl'] = url
+                
+                try:
+                    specBox = browser.find_element_by_id(product['sku_info']['prodPropId'] + product['sku_info']['prodEnumId'])
+                    
+                    storage = None
+                    errTimes = 0
+                    while storage == None and errTimes < 10:
+                        browser.execute_script("arguments[0].click();", specBox)
+                        WebDriverWait(browser, 5).until(lambda x: x.find_element_by_id("productStorage").is_displayed())
+                        
+                        try:
+                            storage_str = browser.find_element_by_id("productStorage").text
+                            storage = storage_re.search(storage_str).group(1)
+                        except Exception as e:
+                            errTimes += 1
+                            print(e)
+                    
+                    if storage == None:
+                        product['visibleStorage'] = '-1'
+                    else:
+                        product['visibleStorage'] = storage
+                except Exception as ee:
+                    product['visibleStorage'] = '0'
+                    print(ee)
+                
+                if int(product['visibleStorage']) <= 20:
+                    storage_warn += "\n" + product['name'] + "仅剩" + product['visibleStorage'] + "件。"
+                    product['is_warning'] = 1
+
         if len(storage_warn) > 0 and get_client_ip(request) == '127.0.0.1':
-            storage_warn = "【重要】请关注以下规格如意金条线上库存！\n" + storage_warn + "\n\n[时间]：" + str(datetime.datetime.now())[0:19]
+            storage_warn = "【重要】请关注以下贵金属产品线上库存！\n" + storage_warn + "\n\n[时间]：" + str(datetime.datetime.now())[0:19]
             
             wechat.send_text_message(2, storage_warn)
             dingding.send_text_message(1, storage_warn)
         
-        return render_template("coding/spider/storage.html", { 'products': products }, request)
+        return render_template("coding/spider/storage.html", { 'products': prods_info }, request)
     else:
         return HttpResponse("非管理员用户禁止访问！")
