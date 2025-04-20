68a708cd8dc54aadc83832f69df90985f7fb2d39
GPLite <GPLite@KIMMY-DESKTOP>
2025-04-20 14:19:11 +0800

    先这么着吧

--- blogsite/coding/spider/cron.py
+++ blogsite/coding/spider/cron.py
@@ -3,23 +3,25 @@ from django.utils import timezone
 from home import models as models_home
 from libs import constants
 
-import argparse
-
 from coding.spider import models as models_code
 from coding.spider import views as views_code
 
 
-def query_storage(*args):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--mode', type=int, help='Mode Parameter')
-    parsed_args = parser.parse_args(args)
-
+def query_storage(mode=constants.NORMAL_MODE):
     dttm = timezone.now()
     msg_level = models_home.message_level.objects.get(id=1)
     
-    if parsed_args.mode == constants.CLEAN_MODE:
+    if mode == constants.CLEAN_MODE:
         models_code.spider_product_storage.objects.filter(event_dt__lt=dttm.date()).delete()
     
     views_code.get_product_details(['9003867817'], msg_level, dttm, True)
 
-    print('{0}: {1}'.format(dttm, parsed_args.mode))
+    print('[{0}] -> {1}'.format(dttm, mode))
+
+
+def query_storage_with_clean():
+    query_storage(mode=constants.CLEAN_MODE)
+
+
+def query_storage_with_save():
+    query_storage(mode=constants.SAVE_MODE)
