c3be3552bcb7a412b94ae56ee0bd939b5f1b173b
GPLite <GPLite@KIMMY-DESKTOP>
2025-04-20 14:04:20 +0800

    retry

--- blogsite/coding/spider/cron.py
+++ blogsite/coding/spider/cron.py
@@ -1,5 +1,3 @@
-import os, django
-
 from django.utils import timezone
 
 from home import models as models_home
@@ -8,26 +6,17 @@ from libs import constants
 from coding.spider import models as models_code
 from coding.spider import views as views_code
 
-from django.conf import settings
-
-
-
-
 
 def query_storage(mode=constants.NORMAL_MODE):
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gplitesite.settings")
-    django.setup()
-    
     dttm = timezone.now()
-#     msg_level = models_home.message_level.objects.get(id=1)
-#     
-#     if mode == constants.CLEAN_MODE:
-#         models_code.spider_product_storage.objects.filter(event_dt__lt=dttm.date()).delete()
-#     
-#     views_code.get_product_details(['9003867817'], msg_level, dttm, True)
+    msg_level = models_home.message_level.objects.get(id=1)
+    
+    if mode == constants.CLEAN_MODE:
+        models_code.spider_product_storage.objects.filter(event_dt__lt=dttm.date()).delete()
+    
+    views_code.get_product_details(['9003867817'], msg_level, dttm, True)
 
     print('[{0}] -> {1}'.format(dttm, mode))
-    print(settings.DATABASES['default'])
 
 
 def query_storage_with_clean():
