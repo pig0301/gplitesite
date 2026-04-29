db2dda8dfeb6804dde9fb0676d7ab2b9667df857
GPLite <GPLite@windows10.microdone.cn>
2026-04-29 11:57:50 +0800

    判断当日是否交易日

--- blogsite/coding/stock/tasks.py
+++ blogsite/coding/stock/tasks.py
@@ -7,14 +7,30 @@ import django_rq, os
 
 @job('ths_worker', timeout=600, result_ttl=86400)
 def init_daily_trade_flag():
+    ths_login()
+    today = timezone.now().date().isoformat()
     
-    return ths_login()
+    # 查询交易日
+    res = THS_DateQueries('SSE', today, today, 'isTradeDay')
+    
+    if res.errorcode == 0:
+        is_trade_day = str(res.data.iloc[0, 0])
+
+        redis_conn = django_rq.get_connection('ths_worker')
+        redis_conn.set('GLOBAL_TRADE_DAY_FLAG', is_trade_day, ex=54000)
+        return f"Done: {is_trade_day}"
+    else:
+        raise Exception(f"iFind Query Error: {res.errormsg}")
 
 
 def ths_login():
     account = os.environ.get('THS_ACCOUNT')
     password = os.environ.get('THS_PASSWORD')
+    
+    if not account or not password:
+        raise Exception("环境变量 THS_ACCOUNT 或 THS_PASSWORD 未配置")
 
     res = THS_iFinDLogin(account, password)
     
-    return res in [0, -202]
+    if res not in [0, -202]:
+        raise Exception(f"iFind 登录失败，错误码: {res}")
