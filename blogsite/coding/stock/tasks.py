68039d8c5f70b012790d6906a4cf8bc55e7c8214
GPLite <GPLite@windows10.microdone.cn>
2026-04-29 11:45:30 +0800

    测试ths登录

--- blogsite/coding/stock/tasks.py
+++ blogsite/coding/stock/tasks.py
@@ -1,9 +1,20 @@
 from django_rq import job
 from django.utils import timezone
+from iFinDPy import *
+
+import django_rq, os
 
 
 @job('ths_worker', timeout=600, result_ttl=86400)
 def init_daily_trade_flag():
-    today = timezone.now().date().isoformat()
     
-    return today
+    return ths_login()
+
+
+def ths_login():
+    account = os.environ.get('THS_ACCOUNT')
+    password = os.environ.get('THS_PASSWORD')
+
+    res = THS_iFinDLogin(account, password)
+    
+    return res in [0, -202]
