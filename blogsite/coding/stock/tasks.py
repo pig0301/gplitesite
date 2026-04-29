924e6f3b13e051ff62d8b1eca3c7b302da36c286
GPLite <GPLite@KIMMY-DESKTOP>
2026-04-30 02:12:17 +0800

    fix！

--- blogsite/coding/stock/tasks.py
+++ blogsite/coding/stock/tasks.py
@@ -27,7 +27,7 @@ def download_daily_quotes():
     
     stock_summary = f"共更新 {ret_stock[0]} 条记录，成功新增 {ret_stock[1]} 条记录"
     quote_summary = f"共删除 {ret_quote[0]} 条记录，成功新增 {ret_quote[1]} 条记录"
-    final_summary = f"①股票信息：{stock_summary}；\r\n\r\n②行情信息：{quote_summary}。"
+    final_summary = f"①股票信息：{stock_summary}；\r\n②行情信息：{quote_summary}。"
     
     wechat.send_text_message(1, final_summary)
 
