f53b7bb53f6813df008a1de5b940655010faf252
GPLite <GPLite@windows10.microdone.cn>
2026-04-29 09:22:14 +0800

    job失效时间为1天

--- blogsite/coding/spider/cron.py
+++ blogsite/coding/spider/cron.py
@@ -8,12 +8,12 @@ icbc_queue = django_rq.get_queue('icbc_worker')
 
 
 def query_storage_as_normal():
-    icbc_queue.enqueue(query_storage, constants.NORMAL_MODE)
+    icbc_queue.enqueue(query_storage, constants.NORMAL_MODE, result_ttl=86400)
 
 
 def query_storage_with_clean():
-    icbc_queue.enqueue(query_storage, constants.CLEAN_MODE)
+    icbc_queue.enqueue(query_storage, constants.CLEAN_MODE, result_ttl=86400)
 
 
 def query_storage_with_save():
-    icbc_queue.enqueue(query_storage, constants.SAVE_MODE)
+    icbc_queue.enqueue(query_storage, constants.SAVE_MODE, result_ttl=86400)
