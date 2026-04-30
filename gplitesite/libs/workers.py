6281da42b4bd37f7a9215c33b8ff29105982f056
GPLite <GPLite@windows10.microdone.cn>
2026-04-30 10:55:35 +0800

    实现worker自动清理（重启）

---
+++ gplitesite/libs/workers.py
@@ -0,0 +1,19 @@
+import os
+from rq import Worker
+
+
+class MaxJobsWorker(Worker):
+    def __init__(self, *args, **kwargs):
+        self.max_jobs = int(os.getenv('RQ_MAX_JOBS', 1))
+        self.jobs_processed = 0
+        super().__init__(*args, **kwargs)
+
+    def perform_job(self, job, queue):
+        rv = super().perform_job(job, queue)
+        self.jobs_processed += 1
+        
+        if self.jobs_processed >= self.max_jobs:
+            self.log.info(f"达到最大处理数 {self.max_jobs}，准备安全退出...")
+            self.request_stop_sigrtmin()
+        
+        return rv
