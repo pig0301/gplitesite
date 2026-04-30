ffe15c95c3ee7db2f94175ed0f06c1a1d860835f
GPLite <GPLite@windows10.microdone.cn>
2026-04-30 11:33:38 +0800

    ceshi

--- gplitesite/libs/workers.py
+++ gplitesite/libs/workers.py
@@ -14,6 +14,7 @@ class MaxJobsWorker(Worker):
         
         if self.jobs_processed >= self.max_jobs:
             self.log.info(f"达到最大处理数 {self.max_jobs}，准备安全退出...")
+            self.log.info(f"{os.getpid()}，{os.getppid()}")
             os.kill(os.getpid(), signal.SIGTERM)
         
         return rv
