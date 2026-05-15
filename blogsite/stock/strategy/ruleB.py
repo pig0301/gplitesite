707c81cebed8f4a3d02b7a2d97e8f1a4044de184
GPLite <GPLite@KIMMY-DESKTOP>
2026-05-15 20:43:41 +0800

    update

--- blogsite/stock/strategy/ruleB.py
+++ blogsite/stock/strategy/ruleB.py
@@ -112,7 +112,8 @@ def pick_stocks_with_macd(tx_date):
 
 def is_good_stock(row):
     isPrice_Rule1 = (row['ma5'] > row['ma10'] > row['ma30'] > row['ma49'] > row['ma60'] > row['ma120'] > row['ma250'])
-    isPrice_Rule2 = (row['ma5'] > row['ma10'] * 1.05 and row['ma10'] > row['ma20'] * 1.05)
-    isPrice_Rule3 = (row['macd'] * 100 / row['close'] < MAX_MACD_RATE)
+    isPrice_Rule2 = (row['ma10'] < row['ma5'] < row['ma10'] * 1.05)
+    isPrice_Rule3 = (row['ma20'] < row['ma10'] < row['ma20'] * 1.05)
+    isMacd_Rule3 = (row['macd'] * 100 / row['close'] < MAX_MACD_RATE)
 
-    return isPrice_Rule1 and isPrice_Rule2 and isPrice_Rule3
+    return isPrice_Rule1 and isPrice_Rule2 and isPrice_Rule3 and isMacd_Rule3
