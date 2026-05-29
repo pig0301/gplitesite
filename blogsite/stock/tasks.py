85220e5be5d3ecad89ad67ac4735fb9eaf2818cc
GPLite <GPLite@windows10.microdone.cn>
2026-05-29 16:16:25 +0800

    test

--- blogsite/stock/tasks.py
+++ blogsite/stock/tasks.py
@@ -22,19 +22,19 @@ def download_daily_quotes():
     ths_login()
     stock_df = get_stocks_list()
     
-    ret_stock = update_stock_info(stock_df)
-    ret_quote = update_daily_quote(stock_df)
+    # ret_stock = update_stock_info(stock_df)
+    # ret_quote = update_daily_quote(stock_df)
     ret_indicator = update_stock_indicators(stock_df)
     THS_iFinDLogout()
     
-    stock_summary = f"共更新 {ret_stock[0]} 条记录，成功新增 {ret_stock[1]} 条记录"
-    quote_summary = f"共删除 {ret_quote[0]} 条记录，成功新增 {ret_quote[1]} 条记录"
-    indicator_summary = f"共删除 {ret_indicator[0]} 条记录，成功新增 {ret_indicator[1]} 条记录"
-    final_summary = f"①股票信息：{stock_summary}；\r\n②行情信息：{quote_summary}；\r\n③指标信息：{indicator_summary}。"
-    
-    wechat.send_text_message(1, final_summary)
+    # stock_summary = f"共更新 {ret_stock[0]} 条记录，成功新增 {ret_stock[1]} 条记录"
+    # quote_summary = f"共删除 {ret_quote[0]} 条记录，成功新增 {ret_quote[1]} 条记录"
+    # indicator_summary = f"共删除 {ret_indicator[0]} 条记录，成功新增 {ret_indicator[1]} 条记录"
+    # final_summary = f"①股票信息：{stock_summary}；\r\n②行情信息：{quote_summary}；\r\n③指标信息：{indicator_summary}。"
+    #
+    # wechat.send_text_message(1, final_summary)
 
-    return final_summary
+    return ret_indicator
 
 
 def update_daily_quote(df):
